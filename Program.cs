8333cf0d617771a517c88fa021da4a22c308a3e8
LiamStewartsait <152663478+LiamStewartsait@users.noreply.github.com>
2024-11-24 21:05:31 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace lab9
+{
+	/// <summary>
+	/// The main entry point for the application.
+	/// </summary>
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
